222535cc456ca6331be9a57fe37c7240468f672c
datadebug57 <kutumoba57@gmail.com>
2025-07-14 07:22:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const GITHUB_TOKEN = "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";
+const GITHUB_REPO = "upload_to_hosting";
+const GITHUB_FILE_PATH = "data/users.json";
+
+const TELEGRAM_TOKEN = "5445531176:AAGwd6pVM-UoDrNos3R00QSlr0KuffkZLMY";
+const TELEGRAM_CHAT_ID = "-1001921678002";
+
+const ADMIN_USER = "superadmin";
+const ADMIN_PASS = "okbro";
